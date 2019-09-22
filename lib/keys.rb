7c54fe637c6074e11221fff4402e367eb7afe3a5
Jalysa Reason <jalysareason@gmail.com>
2019-09-22 14:50:23 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -9,5 +9,18 @@ require 'pry'
 # }
 
 def get_the_keys(groceries)
-  #code your solution here!
-end
+  let :groceries do
+    {
+      dairy: ["milk", "yogurt", "cheese"],
+      vegetable: ["carrots", "broccoli", "cucumbers"],
+      meat: ["chicken", "steak", "salmon"],
+      grains: ["rice", "pasta"]
+    }
+  end
+
+  def get_the_keys(groceries)
+    result = get_the_keys(groceries)
+    puts (result)=([:dairy, :vegetable, :meat, :grains])
+  end
+
+
