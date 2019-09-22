3bd9f7537a743448bc1cf92285ddd60b4a4a43b8
Jalysa Reason <jalysareason@gmail.com>
2019-09-22 14:54:08 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -1,3 +1,17 @@
 def get_the_values(groceries)
-  
+  describe "#get_the_values" do 
+
+  let :groceries do
+    {
+      dairy: ["milk", "yogurt", "cheese"],
+      vegetable: ["carrots", "broccoli", "cucumbers"],
+      meat: ["chicken", "steak", "salmon"],
+      grains: ["rice", "pasta"]
+    }
+  end
+
+  it "returns all the values from the hash" do 
+    result = get_the_values(groceries)
+    expect(result).to eq(["milk", "yogurt", "cheese", "carrots", "broccoli", "cucumbers", "chicken", "steak", "salmon", "rice", "pasta"])
+  end
 end
